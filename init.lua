4efcd8a7939232be93db46e21c36edb6c2eb154b
madddtone <madddtone@gmail.com>
2024-07-28 11:50:07 +0700

    harpponn

--- init.lua
+++ init.lua
@@ -244,6 +244,32 @@ require('lazy').setup({
   --  This is equivalent to:
   --    require('Comment').setup({})
 
+  -- NOTE: Harpoon
+  {
+    'ThePrimeagen/harpoon',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('harpoon').setup {}
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Add file to Harpoon' })
+      vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu, { desc = 'Toggle Harpoon quick menu' })
+      vim.keymap.set('n', '<leader>1', function()
+        ui.nav_file(1)
+      end, { desc = 'Navigate to file 1' })
+      vim.keymap.set('n', '<leader>2', function()
+        ui.nav_file(2)
+      end, { desc = 'Navigate to file 2' })
+      vim.keymap.set('n', '<leader>3', function()
+        ui.nav_file(3)
+      end, { desc = 'Navigate to file 3' })
+      vim.keymap.set('n', '<leader>4', function()
+        ui.nav_file(4)
+      end, { desc = 'Navigate to file 4' })
+    end,
+  },
+
   -- NOTE: nvim tree plugin
   {
     'nvim-tree/nvim-tree.lua',
